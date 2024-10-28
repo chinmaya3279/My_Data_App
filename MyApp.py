7803ca39200a69ac9128d8b6ec45c8897b118d8c
Chinmaya Panigrahi <chinmaya1100@gmail.com>
2024-10-28 20:57:43 +0530

    Exploring Basic Information

--- MyApp.py
+++ MyApp.py
@@ -20,3 +20,24 @@ if file:
         data = pd.read_excel(file)
     st.dataframe(data)
     st.info('File is successfully Uploaded', icon='🚨')
+
+    st.subheader(':rainbow[Basic information of the dataset]',divider='rainbow')
+tab1, tab2, tab3, tab4 = st.tabs(['Summary', 'Top and Bottom Rows', 'Data Types', 'Columns'])
+
+with tab1:
+    st.write(f'There are {data.shape[0]} rows in dataset and  {data.shape[1]} columns in the dataset')
+    st.subheader(':gray[Statistical summary of the dataset]')
+    st.dataframe(data.describe())
+with tab2:
+    st.subheader(':gray[Top Rows]')
+    toprows = st.slider('Number of rows you want', 1, data.shape[0], key='topslider')
+    st.dataframe(data.head(toprows))
+    st.subheader(':gray[Bottom Rows]')
+    bottomrows = st.slider('Number of rows you want', 1, data.shape[0], key='bottomslider')
+    st.dataframe(data.tail(bottomrows))
+with tab3:
+    st.subheader(':grey[Data types of column]')
+    st.dataframe(data.dtypes)
+with tab4:
+    st.subheader('Column Names in Dataset')
+    st.write(list(data.columns))
